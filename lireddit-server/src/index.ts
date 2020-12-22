9426833ebe27d9c34d50ab0802a61cec7a4494e3
KeaghanKennedy <keaghan.kennedy@gmail.com>
2020-12-22 11:17:49 +1100

    feat: Add initial redis setup

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,35 +1,65 @@
 import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import {MikroORM} from "@mikro-orm/core";
+import {__prod__} from "./constants";
 // import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-import { UserResolver } from "./resolvers/user";
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello";
+import {PostResolver} from "./resolvers/post";
+import {UserResolver} from "./resolvers/user";
+
+import * as dotenv from "dotenv";
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import {LiredditDbContext} from "./types";
+
+dotenv.config();
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
-  await orm.getMigrator().up();
+	const orm = await MikroORM.init(microConfig);
+	await orm.getMigrator().up();
 
-  const app = express();
+	const app = express();
 
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver, UserResolver],
-      validate: false,
-    }),
-    context: () => ({ em: orm.em }),
-  });
-  apolloServer.applyMiddleware({ app });
+	const RedisStore = connectRedis(session);
+	const redisClient = redis.createClient();
 
-  app.listen(4000, () => {
-    console.log("Express: Server started on localhost:4000");
-  });
+	app.use(
+		session({
+			name: 'qid',
+			store: new RedisStore({
+				client: redisClient,
+				disableTouch: true
+			}),
+			cookie: {
+				maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+				httpOnly: true,
+				sameSite: 'lax', //TODO look up what csrf is
+				secure: __prod__ //cookie only works in https
+			},
+			secret: '',
+			resave: false,
+		})
+	)
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver, UserResolver],
+			validate: false,
+		}),
+		context: ({req, res}): LiredditDbContext => ({em: orm.em, req, res}),
+	});
+
+	apolloServer.applyMiddleware({app});
+
+	app.listen(4000, () => {
+		console.log("Express: Server started on localhost:4000");
+	});
 };
 
 main().catch((err) => {
-  console.error(err);
+	console.error(err);
 });
--- lireddit-server/src/resolvers/user.ts
+++ lireddit-server/src/resolvers/user.ts
@@ -1,110 +1,113 @@
-import { User } from "../entities/User";
-import { LiredditDbContext } from "src/types";
+import {User} from "../entities/User";
+import {LiredditDbContext} from "src/types";
 import {
-  Arg,
-  Ctx,
-  Field,
-  InputType,
-  Mutation,
-  ObjectType,
-  Resolver,
+	Arg,
+	Ctx,
+	Field,
+	InputType,
+	Mutation,
+	ObjectType,
+	Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
 
 @InputType()
 class UserNamePasswordOptions {
-  @Field()
-  username: string;
+	@Field()
+	username: string;
 
-  @Field()
-  password: string;
+	@Field()
+	password: string;
 }
 
 @ObjectType()
 class FieldError {
-  @Field()
-  field: string;
+	@Field()
+	field: string;
 
-  @Field()
-  message: string;
+	@Field()
+	message: string;
 }
 
 @ObjectType()
 class UserResponse {
-  @Field(() => [FieldError], { nullable: true })
-  errors?: FieldError[];
+	@Field(() => [FieldError], {nullable: true})
+	errors?: FieldError[];
 
-  @Field(() => User, { nullable: true })
-  user?: User;
+	@Field(() => User, {nullable: true})
+	user?: User;
 }
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => UserResponse)
-  async register(
-    @Arg("options") options: UserNamePasswordOptions,
-    @Ctx() { em }: LiredditDbContext
-  ): Promise<UserResponse> {
-    if (options.username.length < 5) {
-      return {
-        errors: [
-          {
-            field: "username",
-            message: "Username must be at least 5 characters",
-          },
-        ],
-      };
-    }
-    if (options.password.length < 8) {
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "Password must be at least 8 characters",
-          },
-        ],
-      };
-    }
-    const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
-    await em.persistAndFlush(user);
-    return {
-      user,
-    };
-  }
+	@Mutation(() => UserResponse)
+	async register(
+		@Arg("options") options: UserNamePasswordOptions,
+		@Ctx() {em}: LiredditDbContext
+	): Promise<UserResponse> {
+		if (options.username.length < 5) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "Username must be at least 5 characters",
+					},
+				],
+			};
+		}
+		if (options.password.length < 8) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "Password must be at least 8 characters",
+					},
+				],
+			};
+		}
+		const hashedPassword = await argon2.hash(options.password);
+		const user = em.create(User, {
+			username: options.username,
+			password: hashedPassword,
+		});
+		await em.persistAndFlush(user);
+		return {
+			user,
+		};
+	}
 
-  @Mutation(() => UserResponse)
-  async login(
-    @Arg("options") options: UserNamePasswordOptions,
-    @Ctx() { em }: LiredditDbContext
-  ): Promise<UserResponse> {
-    const user = await em.findOneOrFail(User, { username: options.username });
-    if (!user) {
-      return {
-        errors: [
-          {
-            field: "username",
-            message: "That user does not exist",
-          },
-        ],
-      };
-    }
-    const validPassword = await argon2.verify(user.password, options.password);
-    if (!validPassword) {
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "Incorrect password",
-          },
-        ],
-      };
-    }
-    return {
-      user,
-    };
-  }
+	@Mutation(() => UserResponse)
+	async login(
+		@Arg("options") options: UserNamePasswordOptions,
+		@Ctx() {em, req}: LiredditDbContext
+	): Promise<UserResponse> {
+		const user = await em.findOneOrFail(User, {username: options.username});
+		if (!user) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "That user does not exist",
+					},
+				],
+			};
+		}
+		const validPassword = await argon2.verify(user.password, options.password);
+		if (!validPassword) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "Incorrect password",
+					},
+				],
+			};
+		}
+
+		//req.session.userId = user.id;
+
+		return {
+			user,
+		};
+	}
 }
